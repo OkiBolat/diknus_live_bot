f8ae7ee572b71129f121dd40014af99471b359cc
okibolat <okuevabubakar@gmail.com>
2025-02-01 01:55:50 +0300

    last

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,14 @@ module.exports = {
   apps: [{
     name: "telegram-livestream-bot",
     script: "livestream_bot.js",
-    watch: true,
+    watch: false,
     max_memory_restart: "1G",
     exp_backoff_restart_delay: 100,
     env: {
       NODE_ENV: "production",
     },
-    restart_delay: 4000,
-    max_restarts: 10,
+    restart_delay: 10000,
+    max_restarts: 5,
     out_file: "logs/out.log",
     error_file: "logs/error.log",
     merge_logs: true,
